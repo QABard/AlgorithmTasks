904fdfda1a3da439626e5a6c4a3b5ac7044d3333
Danil Bardiukov <bardiukov.ds@gmail.com>
2024-04-10 21:08:29 +0400

    Решение первой задачи

--- TaskOne.java
+++ TaskOne.java
@@ -6,14 +6,15 @@ public class MyClass {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         Long sum = 0L;
-        if (number > 0 && number < 2147483647) {
-            for (int i = 1; i < number; i++) {
-                sum = sum + i;
-            }
-            System.out.println(sum);
-        } else {
+        if (number < 1 || number > 2147483646) {
             System.out.println("Необходимо было ввести число больше 0 и меньше 2147483647. Конец программы");
+            return;
         }
+        for (int i = 1; i < number; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
     }
 }
 //Пользователь вводит число с клавиатуры. Число > 0.
