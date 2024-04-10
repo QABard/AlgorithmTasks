bfe7b347a7446c48af30b2b9c1d665b94076f205
Danil Bardiukov <bardiukov.ds@gmail.com>
2024-04-10 20:51:22 +0400

    Решение первой задачи

--- TaskOne.java
+++ TaskOne.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Введите число");
+        System.out.println("Введите число больше нуля");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int sum = 0;
-        for (int i = 1; i < number; i++) {
-            sum = sum + i;
-        }
-        System.out.println(sum);
+        Long sum = 0L;
+        if (number > 0 && number < 2147483647) {
+            for (int i = 1; i < number; i++) {
+                sum = sum + i;
+            }
+            System.out.println(sum);
+        } else {
+            System.out.println("Необходимо было ввести число больше 0 и меньше 2147483647. Конец программы");
         }
     }
+}
 //Пользователь вводит число с клавиатуры. Число > 0.
 // Вывести сумму чисел от 0 до числа НЕ включительно.
