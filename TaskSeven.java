e53914da1bcd7ec588f68ed46817420e32865590
Danil Bardiukov <bardiukov.ds@gmail.com>
2024-04-15 19:12:26 +0400

    Решение седьмой задачи

--- TaskSeven.java
+++ TaskSeven.java
@@ -4,14 +4,14 @@ public class MyClass {
     public static void main(String[] args) {
 
         var scanner = new Scanner(System.in);
-        double maxNumber = 0;
-
+        double maxNumber = -Double.MAX_VALUE;
+        System.out.println("Введите три раза число");
         for (int i = 1; i <= 3; i++) {
-            System.out.println("Введите три раза число больше нуля");
+            System.out.println("Введите число " + i + "-ое число");
             double number = scanner.nextDouble();
 
-            if (number <= 0 || number >= Double.MAX_VALUE) {
-                System.out.println("Необходимо было ввести числа больше 0 и меньше 1.7976931348623157E308. Конец программы");
+            if (number <= -Double.MAX_VALUE || number >= Double.MAX_VALUE) {
+                System.out.println("Необходимо было ввести числа больше 4.9E-324 и меньше 1.7976931348623157E308. Конец программы");
                 return;
             }
 
@@ -19,7 +19,7 @@ public class MyClass {
                 maxNumber = number;
             }
         }
-        System.out.println(maxNumber);
+        System.out.println("Самое большое число " + maxNumber);
     }
 }
 //Даны три числа с плавающей точкой (double, float).
